6f90bbbb96907462b3a6e1802b82da9fee4b28f7
Claudio Geraci <claudio@mosaicoon.com>
2017-09-06 12:52:41 +0200

    rx package name to lowercase

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@
 const express    = require('express');
 const mongoose   = require('mongoose');
 const bodyParser = require('body-parser');
-const Rx         = require('Rx');
+const Rx         = require('rx');
 const fs         = require('fs');
 const path       = require('path');
 
