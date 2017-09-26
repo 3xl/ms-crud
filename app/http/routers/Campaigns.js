81a277fb412cd6b9af436fba5e2ac178164f3a99
Claudio Geraci <claudio@mosaicoon.com>
2017-09-26 15:23:38 +0200

    added list command

--- app/http/routers/Campaigns.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const Controller = require('../controllers/CampaignsController.js');
-const router     = require('./BaseRouter.js');
-
-// Add custom routes here
-
-module.exports = router;
