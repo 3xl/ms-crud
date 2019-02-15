e17445dfa0134fad69832cede318aefecab071f0
Claudio Geraci <geraciclaudio82@gmail.com>
2019-02-15 10:08:15 +0100

    added socket.io support

--- src/Socket.js
+++ src/Socket.js
@@ -19,22 +19,11 @@ class Socket {
    * @memberOf Socket
    */
   constructor(http, config) {
-    this.io = socket(http);
-    
-    /**
-     * Init private events
-     * 
-     */
-    if(config.private && config.private.events.length) {
-      config.private.events.forEach(event => {
-        this.init()
-          .flatMap(socket => Rx.Observable.fromEvent(socket, event.name))
-          .flatMap(event.handler)
-          .subscribe(
-            () => {},
-            error => console.log(error)
-          )
-      });
+    this.io = http ? socket(http) : null;
+
+    if(config) {
+      this.setupPrivateEvents(config);
+      this.setupGlobalEvents(config);
     }
   };
 
@@ -48,6 +37,48 @@ class Socket {
   init() {
     return Rx.Observable.fromEvent(this.io, 'connection')
   };
+
+  /**
+   * Setup private events handlers
+   * 
+   * @param {Object} config 
+   * 
+   * @memberOf Socket
+   */
+  setupPrivateEvents(config) {
+    if(config.private && config.private.events.length) {
+      config.private.events.forEach(event => {
+        this.init()
+          .flatMap(socket => Rx.Observable.fromEvent(socket, event.name))
+          .flatMap(event.handler)
+          .subscribe(
+            () => {},
+            error => console.log(error)
+          )
+      });
+    }
+  }
+
+  /**
+   * Setup global events handlers
+   * 
+   * @param {Object} config 
+   * 
+   * @memberOf Socket
+   */
+  setupGlobalEvents(config) {
+    if(config.global && config.global.events.length) {
+      config.global.events.forEach(event => {
+        this.init()
+          .flatMap(() => Rx.Observable.fromEvent(this.io, event.name))
+          .flatMap(event.handler)
+          .subscribe(
+            () => {},
+            error => console.log(error)
+          )
+      });
+    }
+  }
 };
 
 module.exports = Socket;
